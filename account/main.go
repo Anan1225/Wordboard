9fb005bb372e405aaaa07969abfffdc128e8786b
Anan1225 <1449891717@qq.com>
2022-03-10 15:32:18 +0800

    first commit

---
+++ account/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/Anan1225/memrizr/handler"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	log.Println("Starting server in memrizr...")
+
+	router := gin.New()
+
+	handler.NewHandler(&handler.Config{
+		R: router,
+	})
+
+	// router.GET("/api/account", func(c *gin.Context) {
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 		"hello": "yesyes you find the right place",
+	// 	})
+	// })
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	// Graceful shutdown reference from gin's example:
+	// https://github.com/gin-gonic/examples/blob/master/graceful-shutdown/graceful-shutdown/server.go
+	// Initilize server in goroutine so we can gracefully shut down all connections
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Failed to initialize server: %v\n", err)
+		}
+	}()
+
+	log.Printf("Listening on port %v\n", srv.Addr)
+
+	// Wait for kill signal of channel
+	quit := make(chan os.Signal)
+
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	// This blocks until a signal is passed into the quit channel
+	<-quit
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Shutdown server
+	log.Println("Shutting down server...")
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v\n", err)
+	}
+}
